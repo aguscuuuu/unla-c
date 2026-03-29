40c5796a72d253f7dd2068825e2736b46c97204e
AgustinCuenca <aguabentura08@gmail.com>
2026-03-28 22:47:48 -0300

    commit

--- act-1/1-7.c
+++ act-1/1-7.c
@@ -8,14 +8,15 @@ int main()
     printf("Calcula todos los pares que existen entre dos numeros \n\n");
     printf("Ingresa el primer numero -> ");
     scanf("%d", &a);
+    
     printf("Ingresa el segundo numero (mas grande) -> ");
     scanf("%d", &b);
     printf("\n");
 
     for(int iterador = a ; iterador <= b ; iterador++){   /// Suponiendo que B > A, vamos a ir de A a B
-       if (iterador%2 == 0){   /// Y solo vamos a mostrar los múltiplos de 2
-           printf("%d ", iterador);
-       }
+       if (iterador%2 == 0){   /// Y solo vamos a mostrar los mï¿½ltiplos de 2
+            printf("%d ", iterador);
+        }
     }
     printf("\n");
 
